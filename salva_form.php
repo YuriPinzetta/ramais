d974a3f445a4f170653f563df11a62a1575a8816
yuri <pinzetta@gmail.com>
2015-04-29 13:58:37 +0000

    Feito um lib de funçoes e arrumado salva_form

--- salva_form.php
+++ salva_form.php
@@ -6,14 +6,9 @@
 		return header("HTTP/1.1 404 Bad Request");
 	}
 
-	include "./mysqlconecta.php"; // Conecta ao banco de dados
-	include "./mysqlexecuta.php"; // Executa a clausula SQL
-	
-	//$sql="INSERT INTO 'agenda_online' (local, titulo) VALUES ('".$local."', '".$titulo."')";
-	//$finalizar = mysql_query($sql);
+	include "../lib/functions.php";
 
-	$sql = "INSERT INTO `ramais`.`contato` (`cargos`, `contato`) 
-							VALUES ('$cargo', '$contato');";
-	$res = mysqlexecuta($id,$sql);
+	$conn = db();
+	inserirContato($cargo, $contato, $conn);
 	header('Location: index.php');
 ?>
