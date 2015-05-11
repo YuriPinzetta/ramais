ddfdb0305e9f1f43d0affd5eb6b62a78ba771410
yuri <pinzetta@gmail.com>
2015-05-11 12:50:11 +0000

    começando a usar PDO no php

---
+++ pdo01.php
@@ -0,0 +1,24 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=ramais';
+$username = 'yuri';
+$password = 'Pinzetta';
+$id = 12;
+try {
+	$pdo = new PDO($dsn, $username, $password);
+	$pdo->beginTransaction();
+	$stmt = $pdo->prepare('
+		select * from contato
+		where id = :id
+	');
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute(array(':id' => $id));
+	foreach ($stmt as $row) {
+		print_r($row);
+	}
+	$pdo->commit();
+} catch (PDOException $ex) {
+	$pdo->rollBack();
+	printf("Exception: %s\n", $ex->getMessage());
+	trigger_error($ex->getTraceAsString());
+}
+?>
