103194f7b94817f866c59eeea7e77e443030861f
yuri <pinzetta@gmail.com>
2015-05-22 17:42:54 +0000

    arrumando banco de usuario para login

--- usuario.sql
+++ usuario.sql
@@ -1,2 +1,2 @@
 use ramais;
-create table usuario (id int(10) auto_increment primary key, usuario varchar(100), senha varchar(100)) engine=InnoDB;
+create table usuario (id int(10) auto_increment primary key, login varchar(100), senha varchar(100)) engine=InnoDB;
