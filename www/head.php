ee7fd84074d2b7456ed0f41cd4c965b359653597
yuri <pinzetta@gmail.com>
2015-06-10 18:10:27 +0000

    Limpando o PHP

--- www/head.php
+++
@@ -1,9 +0,0 @@
-<meta charset="UTF-8">
-<title>Ramais</title>
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, user-scalable=no">
-<script src="jquery/jquery.js"></script>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="style.css" rel="stylesheet">
-<script src="bootstrap/js/bootstrap.min.js"></script>
-<script src="validacoes.js"></script>
